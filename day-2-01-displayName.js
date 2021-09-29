7db5908f4dc0b902fc22750ec69d1234175c065e
Mark <mstewart@paddypower.com>
2021-09-29 17:48:53 +0100

    Delete day-2-01-displayName.js

--- day-2-01-displayName.js
+++
@@ -1,9 +0,0 @@
-function displayFullName(){
-
-    let name = ['James Bond','Ada Lovelace', '"Salvador Felipe Jacinto Dalí"']
-    console.log ('My name is ' +(name[0].substring(6)) + '...'  +(name[0]))
-    console.log ('My name is ' +(name[1].substring(4)) + '...'  +(name[1]))
-    console.log ('My name is ' +(name[2].substring(25)) + '...'  +(name[2]))
-}
-
-displayFullName()
